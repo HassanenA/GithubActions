e7976696c926938a1ec63702d3c8c2d1071e5a3a
Hassanen Alyassiri <hassa@hassanens-air-4.unl.edu>
2026-03-04 17:34:14 -0600

    Fix Power method

--- Console/Program.cs
+++ Console/Program.cs
@@ -85,9 +85,9 @@ public class Program
 
 	// Implement this method following a similar pattern as above
 
-	public static double Power(string x, string y)
-{
-    return Math.Pow(double.Parse(x), double.Parse(y));
+  public static double Power(string x, string y)
+     
+    {
+        return Math.Pow(double.Parse(x), double.Parse(y));
+    }
 }
-		return 0.0;
-	}
